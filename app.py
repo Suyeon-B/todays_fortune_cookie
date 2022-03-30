877ef4402a86ada54abb76dc18bdb0e1376c0bd1
Suyeon-B <tndus4243@naver.com>
2022-03-30 11:02:28 +0900

    merge ver. base

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, render_template, request, jsonify, session, redirect, url_for
 from flask_jwt_extended import *
+import hashlib
+import jwt
 # from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
 from pymongo import MongoClient
@@ -19,17 +21,56 @@ def home():
 
 @app.route('/welcome')
 def welcome():
-   if 'userid' in session:
+   # if 'userid' in session:
+   #    return redirect(url_for('logined'))
+   # return render_template('welcome.html')
+
+   # 상호쓰 갖다 붙임
+   token_receive = request.cookies.get('myToken')
+   if token_receive is not None:
       return redirect(url_for('logined'))
+
    return render_template('welcome.html')
+
+SECRET_KEY = 'apple'
 
 @app.route('/login_btn')
 def login_btn():
    return render_template('login.html')
 
+# by 상호
+@app.route('/login', methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user_id = request.form['id_input']
+      user_pw = request.form['pw_input']
+
+      user_pw_hash = hashlib.sha256(user_pw.encode("utf-8")).hexdigest()
+      result = db.users.find_one({'user_id' : user_id, 'user_pwd' : user_pw_hash})
+
+      if result is not None:
+         payload = {
+            'id': user_id,
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes = 60 * 60 * 24)
+         }
+         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+         return jsonify({'success': True, 'token': token})
+      else:
+         return jsonify({'success': False, 'msg': '아이디,비밀번호가 일치하지 않습니다'})
+   else:
+      return render_template('login.html')
+
+
 @app.route('/logined')
 def logined():
-   return render_template('logined.html')
+   token_receive = request.cookies.get('myToken')
+   payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+   user_id = payload['id']
+
+   name = db.users.find_one({'user_id': user_id})
+   userName = name['user_name']
+   return render_template('logined.html', userName = userName)
+   # return render_template('logined.html')
 
 @app.route('/main')
 def main():
@@ -75,7 +116,8 @@ def signIn():
         elif password != re_password:
             return jsonify({'result' : 'fail', 'msg' : "pw error"})
         else:  # 모두 입력이 정상적으로 되었다면 밑에명령실행(DB에 입력됨)
-            pw_hash = bcrypt.generate_password_hash('password')
+            # pw_hash = bcrypt.generate_password_hash('password')
+            pw_hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
             userinfo = {'user_id': userid, 'user_name': username, 'user_pwd': pw_hash }
             db.users.insert_one(userinfo)
             return jsonify({'result' : "success"})
@@ -83,29 +125,29 @@ def signIn():
 
 
 # 로그인
-@app.route('/login', methods=['POST'])
-def login():
+# @app.route('/login', methods=['POST'])
+# def login():
 
-   userid = request.form['userid']
-   password = request.form['password']
+#    userid = request.form['userid']
+#    password = request.form['password']
 
-   # user = db.users.find_one({'user_id': userid}, {'user_pwd': password})
-   user = db.users.find_one({'user_id': userid})
-   pw_check = bcrypt.check_password_hash(user['user_pwd'], 'password')
+#    # user = db.users.find_one({'user_id': userid}, {'user_pwd': password})
+#    user = db.users.find_one({'user_id': userid})
+#    pw_check = bcrypt.check_password_hash(user['user_pwd'], 'password')
 
-   if (user is None) or (pw_check is False):
-      return jsonify({'result' : 'fail'})
-   else:
-      session['userid'] = userid
-      # session['username'] = user['user_name']
-      return jsonify({'result' : "success"})
+#    if (user is None) or (pw_check is False):
+#       return jsonify({'result' : 'fail'})
+#    else:
+#       session['userid'] = userid
+#       # session['username'] = user['user_name']
+#       return jsonify({'result' : "success"})
 
 
 # 로그아웃
-@app.route('/logout')
-def logout():
-   session.pop('userid', None)
-   return redirect('/')
+# @app.route('/logout')
+# def logout():
+#    session.pop('userid', None)
+#    return redirect('/')
 
 
 
