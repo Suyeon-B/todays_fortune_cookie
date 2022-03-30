f647b311abc722394a3f7da51374c9f4c467e910
ktkdgh <ktkdgh821@gmail.com>
2022-03-30 19:17:23 +0900

    save

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
-import os
-from flask import Flask, render_template, request, jsonify, session, redirect, url_for
-from flask_jwt_extended import *
+from flask import Flask, render_template, request, jsonify, redirect, url_for
 import hashlib
 import jwt
-# from flask_wtf.csrf import CSRFProtect
-from flask_bcrypt import Bcrypt
 from pymongo import MongoClient
 import datetime
-# from bson.objectid import ObjectId
 
 app = Flask(__name__)
 client = MongoClient('localhost', 27017)
@@ -78,49 +73,67 @@ def main():
 
 @app.route('/myPage')
 def myPage():
-   return render_template('myPage.html')
+   token_receive = request.cookies.get('myToken')
+   payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+   user_id = payload['id']
+   fortuneList = list(db.saveFortune.find({'user_id' : user_id}))
+   
+   my_fortune = []
+
+   for fortuneLists in fortuneList:
+      my_fortune.append(fortuneLists['fortune'])
+      
+
+   return render_template('myPage.html', my_fortune = my_fortune)
 
 @app.route('/signIn_btn')
 def signIn_btn():
    return render_template('signIn.html')
 
-
-# PW 암호화
-app.config['SECRET_KEY'] = 'galhg2ilh6safbkj'
-app.config['BCRYPT_LEVEL'] = 10
-
-bcrypt = Bcrypt(app)
-
 # 회원가입
 @app.route('/signIn', methods=['GET', 'POST'])
 def signIn():
-    # 회원가입 버튼을 누르면
-    if request.method == 'GET':
-        return render_template("signIn.html")
+   # 회원가입 버튼을 누르면
+   if request.method == 'GET':
+      return render_template("signIn.html")
 
-    # 회원가입완료 버튼을 누르면
-    else:
-        # 회원정보 생성
-        username = request.form['username']
-        userid = request.form['userid']
-        password = request.form['password']
-        re_password = request.form['re_password']
+   # 회원가입완료 버튼을 누르면
+   else:
+      # 회원정보 생성
+      username = request.form['username']
+      userid = request.form['userid']
+      password = request.form['password']
+      re_password = request.form['re_password']
 
-        is_already_exist = db.users.find_one({'user_id' : userid})
+      is_already_exist = db.users.find_one({'user_id' : userid})
 
-        if is_already_exist is not None:
-            return jsonify({'result' : 'fail' , 'msg' : 'alreay exist'})
-        elif not (userid and username and password and re_password):
-            print(userid, username, password, re_password)
-            return jsonify({'result' : 'fail' , 'msg' : 'fill error'})
-        elif password != re_password:
-            return jsonify({'result' : 'fail', 'msg' : "pw error"})
-        else:  # 모두 입력이 정상적으로 되었다면 밑에명령실행(DB에 입력됨)
-            # pw_hash = bcrypt.generate_password_hash('password')
-            pw_hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
-            userinfo = {'user_id': userid, 'user_name': username, 'user_pwd': pw_hash }
-            db.users.insert_one(userinfo)
-            return jsonify({'result' : "success"})
+      if is_already_exist is not None:
+         return jsonify({'result' : 'fail' , 'msg' : 'alreay exist'})
+      elif not (userid and username and password and re_password):
+         print(userid, username, password, re_password)
+         return jsonify({'result' : 'fail' , 'msg' : 'fill error'})
+      elif password != re_password:
+         return jsonify({'result' : 'fail', 'msg' : "pw error"})
+      else:  # 모두 입력이 정상적으로 되었다면 밑에명령실행(DB에 입력됨)
+         # pw_hash = bcrypt.generate_password_hash('password')
+         pw_hash = hashlib.sha256(password.encode("utf-8")).hexdigest()
+         userinfo = {'user_id': userid, 'user_name': username, 'user_pwd': pw_hash }
+         db.users.insert_one(userinfo)
+         return jsonify({'result' : "success"})
+
+# 메인페이지에서 포춘쿠키 저장하기
+@app.route('/save', methods=['POST'])
+def save():
+   fortune = request.form['fortune']
+   myToken = request.form['myToken']
+   payload = jwt.decode(myToken, SECRET_KEY, algorithms=['HS256'])['id']
+   
+   fortuneInfo = {'user_id' : payload, 'fortune' : fortune}
+   db.saveFortune.insert_one(fortuneInfo)
+   return jsonify({'result' : "success"})
+
+
+
 
 
 
